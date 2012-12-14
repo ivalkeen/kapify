ec188ed6952dbc67abe7268927664ba6876cf976
Ivan Tkalin <itkalin@gmail.com>
2012-12-15 02:59:33 +0400

    fix templates path

--- lib/kapify/base.rb
+++ lib/kapify/base.rb
@@ -20,7 +20,7 @@ Capistrano::Configuration.instance.load do
     end
   end
 
-  set_default(:templates_path, "config/deploy/templates")
+  set_default(:templates_path, "#{Dir.pwd}/config/deploy/templates")
 
   def template(from, to)
     erb = File.read(File.expand_path("#{templates_path}/#{from}", __FILE__))
